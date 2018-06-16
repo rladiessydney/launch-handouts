377e8bfdc8e9cfe854a9e35e6cbf469b3b43b505
Dani Navarro <d.navarro@unsw.edu.au>
2018-06-16 18:33:10 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# R-Ladies Sydney launch handout
+
+
+- `handout.Rmd`, when knit, writes a pdf handout with a randomly generated bingo card for the icebreaker
+- `big_doc.R` renders the `handout.Rmd` file multiple times, then merges to a single multi-page pdf
---
+++ big_doc.R
@@ -0,0 +1,16 @@
+library(rmarkdown)
+library(tabulizer)
+
+npages <- 5
+files <- paste0("./temp_handout",1:npages,".pdf")
+
+for(i in 1:npages) {
+  render(
+    input = "./handout.Rmd", 
+    output_format = "all",
+    output_file = files[i]
+  )
+} 
+
+merge_pdfs(files,"handouts.pdf")
+file.remove(files)
